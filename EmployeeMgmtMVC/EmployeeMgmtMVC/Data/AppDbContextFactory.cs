a6c5a31eb02182a3606f31a70ad2b9b4303cf964
Arjunmurali7 <146719835+Arjunmurali7@users.noreply.github.com>
2025-09-08 18:49:44 +0530

    mmm

---
+++ EmployeeMgmtMVC/EmployeeMgmtMVC/Data/AppDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EmployeeMgmtMVC.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            // 👇 Use the same connection string as in appsettings.json
+            var connectionString = "server=localhost;port=3306;database=EmployeeDb3;user=root;password=arjun";
+
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
